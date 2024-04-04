a5262f20cb70b5f0d37660c6786167a68fb2bd02
Tapomoy Das <160303114+TAPOMOY1994@users.noreply.github.com>
2024-04-04 20:01:16 +0530

    Add files via upload

---
+++ HOISTING.js
@@ -0,0 +1,31 @@
+
+console.log(a);
+var a=5;
+
+
+console.log(b);
+let b=7;
+
+
+console.log(c);
+const c=2;
+
+
+abc();
+
+function abc()
+{
+
+    console.log("HELLO");
+}
+
+
+variable();
+
+var variable=function(){
+
+
+    console.log("I AM DOING WELL");
+}
+
+variable();
